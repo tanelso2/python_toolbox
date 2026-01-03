d22e8308d60b0f815b8c2ff56700d5f4163fea19
Thomas Nelson <tanelso2@gmail.com>
2026-01-02 18:56:23 -0600

    Rename deftest to fn_test

--- test/toolbox_test/test_tools_test.py
+++ test/toolbox_test/test_tools_test.py
@@ -1,11 +1,11 @@
-from toolbox.test_tools import deftest, ExpectedException, FuncParams
+from toolbox.test_tools import fn_test, ExpectedException, FuncParams
 
 
 def add(x, y):
     return x + y
 
 
-@deftest()
+@fn_test()
 def test_add():
     return [
         ((1, 2), 3),
@@ -18,7 +18,7 @@ def divide(x, y):
     return x / y
 
 
-@deftest()
+@fn_test()
 def test_divide():
     return [
         ((4, 2), 2),
@@ -32,7 +32,7 @@ def test_divide():
     ]
 
 
-@deftest(f=divide)
+@fn_test(f=divide)
 def test_divide_floats():
     return [
         ((5.0, 2.0), 2.5),
